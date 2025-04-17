98e53c0d08b46fb822c5739116aefb7fe8db32b2
Ray Ramirez <rramirez70@gatech.edu>
2025-04-17 16:30:57 -0400

    Social feed and profile creation
    This is the social feed and the profile creation

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Profile
+from .forms import ProfileForm
 
-# Create your views here.
+@login_required
+def profile_detail(request):
+    return render(request, 'profiles/profile_detail.html', {
+        'profile': request.user.profile
+    })
+
+@login_required
+def profile_edit(request):
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_detail')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'profiles/profile_edit.html', {
+        'form': form
+    })
