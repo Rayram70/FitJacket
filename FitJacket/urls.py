72dc7f2174652f559a78a986139905dcec928e20
pranavraju24 <161873597+pranavraju24@users.noreply.github.com>
2025-04-17 16:40:40 -0400

    Update urls.py
    removed markers

--- FitJacket/urls.py
+++ FitJacket/urls.py
@@ -27,15 +27,13 @@ urlpatterns = [
 
         path('accounts/', include('allauth.urls')),
         path('', include('profiles.urls')),
-
- posts
+    
         path('groups/', include('groups.urls')),
 
         path('workouts/', include('workouts.urls', namespace='workouts')),
 
         path('social/', include('social.urls')),
 
-main
 
 
 ]
