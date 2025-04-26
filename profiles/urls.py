7280d1e2f3b4b754f7d1df39fc097bce5192fc4a
Ray Ramirez <rramirez70@gatech.edu>
2025-04-26 00:37:03 -0400

    Likes and friends list

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'profiles'
 urlpatterns = [
     path('profile/', views.profile_detail, name='profile_detail'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('user/<str:username>/', views.public_profile, name='public_profile'),
+
 ]
